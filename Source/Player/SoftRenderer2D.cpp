46491c7b64af6787c79d6b4e1ef901632d8e10c9
tmddn5645 <tmddn5645@gmail.com>
2025-12-28 01:16:56 +0900

    4-2
    4-2

--- Source/Player/SoftRenderer2D.cpp
+++ Source/Player/SoftRenderer2D.cpp
@@ -92,6 +92,7 @@ void SoftRenderer::Render2D()
 	float rad = 0.f;
 	static float increment = 0.001f;
 	static std::vector<Vector2> hearts;
+	HSVColor hsv(0.f, 1.f, 0.85f);
 
 	// 하트를 구성하는 점 생성
 	if (hearts.empty())
@@ -109,9 +110,12 @@ void SoftRenderer::Render2D()
 		}
 	}
 
+	rad = 0.f;
 	for (auto const& v : hearts)
 	{
-		r.DrawPoint(v * currentScale + currentPosition, LinearColor::Blue);
+		hsv.H = rad / Math::TwoPI;
+		r.DrawPoint(v * currentScale + currentPosition, hsv.ToLinearColor());
+		rad += increment;
 	}
 
 	// 현재 위치와 스케일을 화면에 출력
