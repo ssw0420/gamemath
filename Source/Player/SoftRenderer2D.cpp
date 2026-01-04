91a393fb289cf9af84aef259bfa4fbe7be1c64cd
tmddn5645 <tmddn5645@gmail.com>
2026-01-04 21:24:58 +0900

    5-1
    5-1

--- Source/Player/SoftRenderer2D.cpp
+++ Source/Player/SoftRenderer2D.cpp
@@ -114,12 +114,32 @@ void SoftRenderer::Render2D()
 		}
 	}
 
+	float sin = 0.f, cos = 0.f;
+	Math::GetSinCos(sin, cos, currentDegree);
+
+	Vector2 rBasis1(cos, sin);
+	Vector2 rBasis2(-sin, cos);
+	Matrix2x2 rMatrix(rBasis1, rBasis2);
+
+	Vector2 sBasis1 = Vector2::UnitX * currentScale;
+	Vector2 sBasis2 = Vector2::UnitY * currentScale;
+	Matrix2x2 sMatrix(sBasis1, sBasis2);
+
+	Matrix2x2 finalMatrix = rMatrix * sMatrix;
+
 	// 각 값을 초기화한 후 색상을 증가시키면서 점에 대응
 	rad = 0.f;
 	for (auto const& v : hearts)
 	{
+		// 1. 점에 행렬 적용
+		Vector2 transformedV = finalMatrix * v;
+
+		// 2. 변환된 점 이동
+		Vector2 translatedV = transformedV + currentPosition;
+
 		hsv.H = rad / Math::TwoPI;
-		r.DrawPoint(v, hsv.ToLinearColor());
+		r.DrawPoint(translatedV, hsv.ToLinearColor());
+
 		rad += increment;
 	}
 
